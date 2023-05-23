e4fc4fa59fc217c9510f68ab7bf9661b883d38c2
gustavo <gustavoferreira.png@gamil.com>
2023-05-22 22:48:52 -0300

    update

--- 05-banco-de-dados-com-pdo/05-05-explorando-estilos-de-busca/index.php
+++ 05-banco-de-dados-com-pdo/05-05-explorando-estilos-de-busca/index.php
@@ -11,12 +11,41 @@ use Source\Database\Connect;
  */
 fullStackPHPClassSession("fetch", __LINE__);
 
+$connect = Connect::getInstance();
+$read = $connect->query("SELECT * FROM users LIMIT 3");
+
+if (!$read->rowCount()) {
+    echo "<p class='trigger warning'>Não obteve resultados.</p>";
+} else {
+    //var_dump($read->fetch());
+
+    //fetch busca o primeiro registro mas é possível percorrer o resultado da query com laços de repetição
+    while ($user = $read->fetch()) {
+        var_dump($user);
+    }
+
+    var_dump($read->fetch());
+}
+
 
 /*
  * [ fetch all ] http://php.net/manual/pt_BR/pdostatement.fetchall.php
  */
 fullStackPHPClassSession("fetch all", __LINE__);
 
+//depois do valor do limit se for colocado ,valor é o offset após o limit
+$read = $connect->query("SELECT * FROM users LIMIT 3,2");
+
+//while ($user = $read->fetchAll()) {
+//    var_dump($user);
+//}
+
+//para o fetchAll utiliza-se o foreach
+foreach ($read->fetchAll() as $user) {
+    var_dump($user);
+}
+
+var_dump($read->fetchAll());
 
 /*
  * [ fetch save ] Realziar um fetch diretamente em um PDOStatement resulta em um clear no buffer da consulta. Você
@@ -24,10 +53,40 @@ fullStackPHPClassSession("fetch all", __LINE__);
  */
 fullStackPHPClassSession("fetch save", __LINE__);
 
+$read = $connect->query("SELECT * FROM users LIMIT 5,1");
+$result = $read->fetchAll();
+
+var_dump(
+    $read->fetchAll(),
+    $result,
+    $result
+);
 
 /*
  * [ fetch modes ] http://php.net/manual/pt_BR/pdostatement.fetch.php
  */
 fullStackPHPClassSession("fetch styles", __LINE__);
 
+$read = $connect->query("SELECT * FROM users LIMIT 1");
+foreach ($read->fetchAll() as $user) {
+    var_dump($user, $user->first_name);
+}
 
+$read = $connect->query("SELECT * FROM users LIMIT 1");
+foreach ($read->fetchAll(PDO::FETCH_NUM) as $user) {
+    var_dump($user, $user[1]);
+}
+
+$read = $connect->query("SELECT * FROM users LIMIT 1");
+foreach ($read->fetchAll(PDO::FETCH_ASSOC) as $user) {
+    var_dump($user, $user['first_name']);
+}
+
+//criando classe apartir do FETCH_CLASS - uma classe que representa uma tabela
+$read = $connect->query("SELECT * FROM users LIMIT 1");
+foreach ($read->fetchAll(PDO::FETCH_CLASS, \Source\Database\Entity\UserEntity::class) as $user) {
+    /**
+     * @var \Source\Database\Entity\UserEntity $user
+     */
+    var_dump($user, $user->getFirstName());
+}
--- 05-banco-de-dados-com-pdo/05-06-desmistificando-transacoes/index.php
+++ 05-banco-de-dados-com-pdo/05-06-desmistificando-transacoes/index.php
@@ -22,3 +22,25 @@ use Source\Database\Connect;
  * persistir no banco de dados (Uma transação só tem sentido se houver gravação)
  */
 fullStackPHPClassSession("transaction", __LINE__);
+
+try {
+    $pdo = Connect::getInstance();
+    $pdo->beginTransaction();
+
+    $pdo->query("
+    INSERT INTO users (first_name, last_name, email, document) 
+    VALUES ('Gustavo', 'Ferreira', 'gustavoferreira.png@gmail.com', '12345678');
+    ");
+    $userId = $pdo->lastInsertId();
+
+    $pdo->query("
+    INSERT INTO users_address (user_id, street, number, complement) 
+    VALUES ('{$userId}', 'Av. Paulista', '123', 'Sala 2');
+    ");
+
+    $pdo->commit();
+    echo "<p class='trigger accept'>Cadastro com sucesso!</p>";
+} catch (PDOException $exception) {
+    $pdo->rollBack();
+    var_dump($exception);
+}
